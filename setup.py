0234d3854a13c7aa4fe949037ab342df82765366
AdityaPrashantPashte <prashant@authnull.com>
2025-12-15 15:45:00 +0530

    test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AuthSec_SDK",
-    version="3.3.0",
+    version="3.5.0",
     description="AuthSec SDK for MCP_AUTH, SERVICES, and SPIRE integration",
     author="AuthSec Team",
     email="a@authnull.com",
