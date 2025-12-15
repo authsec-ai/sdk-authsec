3af966fbbad845a443cde57d70463c83b20daefe
AdityaPrashantPashte <prashant@authnull.com>
2025-12-15 15:39:35 +0530

    test

--- AuthSec_SDK/spiffe_workload_api/api/workload_pb2_grpc.py
+++ AuthSec_SDK/spiffe_workload_api/api/workload_pb2_grpc.py
@@ -3,9 +3,9 @@
 import grpc
 import warnings
 
-from . import workload_pb2 as workload__pb2
+import workload_pb2 as workload__pb2
 
-GRPC_GENERATED_VERSION = '1.75.1'
+GRPC_GENERATED_VERSION = '1.69.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
